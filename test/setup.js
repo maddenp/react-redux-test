fd7a4dff95cd7985fbd4aeb9bdf87a3aa6046fcb
Paul Madden <maddenp@colorado.edu>
2016-02-24 10:29:04 -0700

    Simplify setup.js (no need for jsdom)

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,4 @@
 import chai from 'chai';
 import spies from 'chai-spies';
-import { jsdom } from 'jsdom';
 
 chai.use(spies);
-
-global.document = jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
-global.navigator = global.window.navigator;
