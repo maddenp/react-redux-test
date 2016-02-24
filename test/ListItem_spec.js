b295812f45a5bbda0a36baddf6b1d755b0f9430e
Paul Madden <maddenp@colorado.edu>
2016-02-24 06:12:36 -0700

    Add skeleton spec for ListItem

---
+++ test/ListItem_spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import { expect } from 'chai';
+
+import ListItem from '../src/components/ListItem';
+
+describe('ListItem', () => {
+  it('should pass this trivial test', () => {
+    expect(true).to.equal(true);
+  });
+});
