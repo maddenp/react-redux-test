22aff85decdae4c3da697817b27b2cb22525f999
Paul Madden <maddenp@colorado.edu>
2016-02-24 05:16:49 -0700

    Do not add empty items, clear box after add

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,8 +1,9 @@
 export default (state, action) => {
   switch (action.type) {
   case 'ADD':
+    if (state.box_value.length === 0) return state;
     const new_item = {key: state.items[state.items.length - 1].key + 1, val: state.box_value};
-    return Object.assign({}, state, {items: state.items.slice().concat(new_item)});
+    return Object.assign({}, state, {items: state.items.slice().concat(new_item), box_value: ''});
   case 'ENTRY':
     return Object.assign({}, state, {box_value: action.val});
   case 'REMOVE':
