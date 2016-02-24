f8363bf74a6231b1382634cccb8b71ed93cf921f
Paul Madden <maddenp@colorado.edu>
2016-02-24 07:09:47 -0700

    Work on ListItem spec

--- test/ListItem_spec.js
+++ test/ListItem_spec.js
@@ -1,11 +1,23 @@
+import chai from 'chai';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
 
 import ListItem from '../src/components/ListItem';
 
+const setup = () => {
+  const renderer = TestUtils.createRenderer();
+  renderer.render(
+    <ListItem key={88} item={'word'} onItemClick={chai.spy(key => ({type: 'a', key: key}))} />
+  );
+  return renderer.getRenderOutput();
+};
+
 describe('ListItem', () => {
-  it('should pass this trivial test', () => {
-    expect(true).to.equal(true);
+
+  it('is a <li>', () => {
+    const output = setup();
+    expect(output.type).to.equal('li');
   });
+
 });
