bff71f9ec81c97a4dbd38a198275b5556c2ced68
Paul Madden <maddenp@colorado.edu>
2016-03-06 18:57:15 -0700

    Fix error is reducer dispatching

--- reducer.js
+++ reducer.js
@@ -1,20 +1,25 @@
 /* jshint esversion: 6 */
 
-const ADD = state => {
-  if (state.box_value.length === 0) return state;
-  const new_item = {key: state.items[state.items.length - 1].key + 1, val: state.box_value};
-  return Object.assign({}, state, {items: state.items.slice().concat(new_item), box_value: ''});
-};
-
-const ENTRY = state => (
-  Object.assign({}, state, {box_value: action.val})
-);
-
-const REMOVE = state => (
-  Object.assign({}, state, {items: state.items.reduce((m, e) => e.key === action.key ? m : m.concat(e), [])})
-);
-
 export default (state, action) => {
-  const f = Object.keys[action.type];
-  return typeof(f) === 'function' ? f(state) : state;
+
+  const actions = {
+
+    'ADD': state => {
+      if (state.box_value.length === 0) return state;
+      const new_item = {key: state.items[state.items.length - 1].key + 1, val: state.box_value};
+      return Object.assign({}, state, {items: state.items.slice().concat(new_item), box_value: ''});
+    },
+
+    'ENTRY': state => (
+      Object.assign({}, state, {box_value: action.val})
+    ),
+
+    'REMOVE': state => (
+      Object.assign({}, state, {items: state.items.reduce((m, e) => e.key === action.key ? m : m.concat(e), [])})
+    )
+
+  };
+  
+  return actions[action.type] ? actions[action.type](state) : state;
+
 };
