4e86fa5d47759b2a49bb01c2ff8805f0d892d37c
Paul Madden <maddenp@colorado.edu>
2016-03-06 18:15:14 -0700

    In reducer, replace switch with function dispatch on action name

--- reducer.js
+++ reducer.js
@@ -1,14 +1,20 @@
+/* jshint esversion: 6 */
+
+const ADD = state => {
+  if (state.box_value.length === 0) return state;
+  const new_item = {key: state.items[state.items.length - 1].key + 1, val: state.box_value};
+  return Object.assign({}, state, {items: state.items.slice().concat(new_item), box_value: ''});
+};
+
+const ENTRY = state => (
+  Object.assign({}, state, {box_value: action.val})
+);
+
+const REMOVE = state => (
+  Object.assign({}, state, {items: state.items.reduce((m, e) => e.key === action.key ? m : m.concat(e), [])})
+);
+
 export default (state, action) => {
-  switch (action.type) {
-  case 'ADD':
-    if (state.box_value.length === 0) return state;
-    const new_item = {key: state.items[state.items.length - 1].key + 1, val: state.box_value};
-    return Object.assign({}, state, {items: state.items.slice().concat(new_item), box_value: ''});
-  case 'ENTRY':
-    return Object.assign({}, state, {box_value: action.val});
-  case 'REMOVE':
-    return Object.assign({}, state, {items: state.items.reduce((m, e) => e.key === action.key ? m : m.concat(e), [])});
-  default:
-    return state;
-  }
-}
+  const f = Object.keys[action.type];
+  return typeof(f) === 'function' ? f(state) : state;
+};
